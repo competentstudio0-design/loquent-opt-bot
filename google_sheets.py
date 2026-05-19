350c8a6efd468e7ba4384e4749ac258b974ef799
competentstudio0-design <competentstudio0@gmail.com>
2026-05-19 21:45:18 +0300

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -1,31 +1,4 @@
-# google_sheets.py
-
 import json
-import os
-import gspread
-from google.oauth2.service_account import Credentials
-
-SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
-
-def get_sheets():
-    creds_dict = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
-
-    creds = Credentials.from_service_account_info(
-        creds_dict,
-        scopes=SCOPES
-    )
-
-    client = gspread.authorize(creds)
-
-    spreadsheet = client.open_by_key(
-        "17fAEH1Gn-vpjy90V9dwJGe6ADSQfixdOSBlGTbOb-Lk"
-    )
-
-    return (
-        import json
 import os
 import gspread
 from google.oauth2.service_account import Credentials
@@ -57,7 +30,3 @@ def init_sheets():
 
     products_sheet = spreadsheet.worksheet("products")
     orders_sheet = spreadsheet.worksheet("orders")
-    )
-
-
-products_sheet, orders_sheet = get_sheets()
