1136bbce8db10316c81d70cf09af614ae0427950
competentstudio0-design <competentstudio0@gmail.com>
2026-05-19 18:16:53 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,12 @@
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton
+)
 
+main_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="📦 Каталог")],
+        [KeyboardButton(text="🛒 Мои заказы")]
+    ],
+    resize_keyboard=True
+)
