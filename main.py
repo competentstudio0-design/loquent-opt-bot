f8a853a9a23d7f1d5c57a7f6b7de269bd054d478
competentstudio0-design <competentstudio0@gmail.com>
2026-05-19 07:26:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ async def catalog(message: Message):
     await message.answer(text)
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
