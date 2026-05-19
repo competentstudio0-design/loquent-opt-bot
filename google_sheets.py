1a32beb7a0415ba47bf8f80e5c6e8c5d5bfffdeb
competentstudio0-design <competentstudio0@gmail.com>
2026-05-19 07:07:47 +0300

    Create google_sheets.py

---
+++ google_sheets.py
@@ -0,0 +1,20 @@
+import json
+import os
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds_dict = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+
+creds = Credentials.from_service_account_info(
+    creds_dict,
+    scopes=SCOPES
+)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("НАЗВАНИЕ_ТАБЛИЦЫ").sheet1
